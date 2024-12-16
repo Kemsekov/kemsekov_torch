5ad771e63782589cb6f3d721280d15cf4df9c56b
Vlad Kemsekov <pikova98@bk.ru>
2024-12-16 10:04:25 +0500

    Update resunet.py

--- resunet.py
+++ resunet.py
@@ -88,7 +88,7 @@ class Decoder(torch.nn.Module):
 
     def forward(self,x: torch.Tensor,skip_connections : List[torch.Tensor]):
         # Upsampling path
-        for i, (up,conv_1x1) in enumerate(zip(self.ups,self.up_1x1_convs)):
+        for i, up in enumerate(self.ups):
             x = up(x)
             if len(skip_connections)!=0:
                 # Concatenate the corresponding skip connection (from the downsampling path)
@@ -98,9 +98,10 @@ class Decoder(torch.nn.Module):
                 x = torch.cat((x, skip), dim=1)
                 
                 # to decrease num of channels
-                x = conv_1x1(x)
+                x = self.up_1x1_convs[i](x)
         x = self.up5(x)
         return x
+    
     
 class ResidualUnet(torch.nn.Module):
     # output_scale must be power of 2: 0.125 0.25 0.5 1 2 4 etc
