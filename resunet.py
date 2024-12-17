edd7a295a6d104c599ec27883a649493d3b312ed
Vlad Kemsekov <pikova98@bk.ru>
2024-12-17 14:08:08 +0500

    Update resunet.py

--- resunet.py
+++ resunet.py
@@ -37,7 +37,7 @@ class Encoder(torch.nn.Module):
         return x
 
 class Decoder(torch.nn.Module):
-    def __init__(self, up_in_channels, up_out_channels, up_block_sizes, ups_conv2d_impl, output_scale, out_channels):
+    def __init__(self, up_in_channels, up_out_channels, up_block_sizes, ups_conv2d_impl):
         super().__init__()
         ups = []
         conv1x1s = []
@@ -56,35 +56,9 @@ class Decoder(torch.nn.Module):
 
             ups.append(up_i)
             conv1x1s.append(conv1x1_i)
-
-        self.ups =          torch.nn.ModuleList(ups)
-        self.up_1x1_convs = torch.nn.ModuleList(conv1x1s)
-
-        if output_scale<1:
-            kernel_size = 3
-            # for smaller scales we need to use larger kernel
-            if output_scale<0.25:
-                kernel_size = 5
-            self.up5 = ResidualBlock(
-                in_channels=up_in_channels[-1],
-                out_channels=out_channels,
-                kernel_size=kernel_size,
-                stride = int(0.5/output_scale),
-                dilation=1,
-                repeats=up_block_sizes[-1],
-                conv2d_impl=[nn.Conv2d]+[BSConvU]*(up_block_sizes[-1]-1)
-            )
-        
-        if output_scale==1:
-            self.up5 = ResidualBlock(
-                in_channels=up_in_channels[-1],
-                out_channels=out_channels,
-                kernel_size=3,
-                stride = 2,
-                dilation=1,
-                repeats=up_block_sizes[-1],
-                conv2d_impl=[torch.nn.ConvTranspose2d]+[BSConvU]*(up_block_sizes[-1]-1)
-            )
+        self.ups =          torch.nn.ModuleList(ups[:-1])
+        self.up_1x1_convs = torch.nn.ModuleList(conv1x1s[:-1])
+        self.up5 = ups[-1][0]
 
     def forward_with_skip(self,x: torch.Tensor,skip_connections : List[torch.Tensor]):
         # Upsampling path
@@ -113,26 +87,28 @@ class Decoder(torch.nn.Module):
 class ResidualUnet(torch.nn.Module):
     # output_scale must be power of 2: 0.125 0.25 0.5 1 2 4 etc
     # it defines what size of output tensor should be relative to input tensor
-    def __init__(self,in_channels=3, out_channels = 1, block_sizes=[2,2,2,2,2],output_scale = 1):
+    def __init__(self,in_channels=3, out_channels = 3, block_sizes=[2,2,2,2,2],output_scale = 1):
         super().__init__()
-        assert output_scale>=0.125 and output_scale<=1, "output_scale must be in range [0.125,1]"
-
         in_channels_ =  [in_channels,64, 128, 192, 256]
-        out_channels_ = [64,         128,192, 256, 256]
+        out_channels_ = [64,         128,192, 256, 320]
         dilations=[
             1,
             1,
             [1]*128+[2]*64,
             [1]*128+[2]*64+[3]*64,
-            [1]*128+[2]*64+[3]*64
+            [1]*192+[2]*64+[3]*64
         ]
+        self.scaler = nn.Upsample(scale_factor=output_scale)
+        
+        if output_scale==1:
+            self.scaler = nn.Identity()
 
         downs_conv2d_impl = [
             [nn.Conv2d]+[BSConvU]*(block_sizes[0]-1),#nn.Conv2d,
             [nn.Conv2d]+[BSConvU]*(block_sizes[1]-1),#nn.Conv2d,
             [nn.Conv2d]+[BSConvU]*(block_sizes[2]-1),#nn.Conv2d,
             [nn.Conv2d]+[BSConvU]*(block_sizes[3]-1),#nn.Conv2d,
-            [nn.Conv2d]+[BSConvU]*(block_sizes[4]-1)#nn.Conv2d,
+            [nn.Conv2d]+[BSConvU]*(block_sizes[4]-1),#nn.Conv2d,
         ]
 
         up_block_sizes = block_sizes[::-1]
@@ -140,14 +116,18 @@ class ResidualUnet(torch.nn.Module):
             [nn.ConvTranspose2d]+[BSConvU]*(up_block_sizes[0]-1),#nn.ConvTranspose2d,#
             [nn.ConvTranspose2d]+[BSConvU]*(up_block_sizes[1]-1),#nn.ConvTranspose2d,#
             [nn.ConvTranspose2d]+[BSConvU]*(up_block_sizes[2]-1),#nn.ConvTranspose2d,#
-            [nn.ConvTranspose2d]+[BSConvU]*(up_block_sizes[3]-1)#nn.ConvTranspose2d,#
+            [nn.ConvTranspose2d]+[BSConvU]*(up_block_sizes[3]-1),#nn.ConvTranspose2d,#
+            nn.ConvTranspose2d,# make last layer full to get best reconstruction performance
         ]
         up_in_channels = out_channels_[::-1]
         up_out_channels = in_channels_ [::-1]
+        up_out_channels[-1]=out_channels
         self.encoder = Encoder(in_channels_,out_channels_,dilations,block_sizes,downs_conv2d_impl)
-        self.decoder = Decoder(up_in_channels,up_out_channels,up_block_sizes,ups_conv2d_impl,output_scale,out_channels)
+        self.decoder = Decoder(up_in_channels,up_out_channels,up_block_sizes,ups_conv2d_impl)
 
     def forward(self, x):
         x,skip = self.encoder.forward_with_skip(x)
+        x=self.scaler(x)
+        skip = [self.scaler(i) for i in skip]
         x = self.decoder.forward_with_skip(x,skip)
         return x
