532bc3a2a4550a3b483718058d52d3e78bf28389
Vlad Kemsekov <pikova98@bk.ru>
2024-12-25 18:05:49 +0500

    Update common_modules.py

--- common_modules.py
+++ common_modules.py
@@ -1,7 +1,8 @@
 import torch.nn.functional as F
+import torch
 
 # change tensor shape
-class Interpolate(nn.Module):
+class Interpolate(torch.nn.Module):
     def __init__(self, scale_factor=None, size=None, mode='nearest', align_corners=None):
         super(Interpolate, self).__init__()
         self.scale_factor = scale_factor
