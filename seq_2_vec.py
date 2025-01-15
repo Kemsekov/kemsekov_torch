844ca4994b901a6972afd534313928951faea418
Kemsekov <pikova98@bk.ru>
2025-01-15 14:15:10 +0500

    improve seq2vec

--- seq_2_vec.py
+++ seq_2_vec.py
@@ -1,9 +1,10 @@
 from kemsekov_torch.residual import ResidualBlock
 from kemsekov_torch.positional_emb import *
-# from pos_encodings import PositionalEncoding1D
+from kemsekov_torch.conv_modules import SCSEModule
+
 import torch
 import torch.nn as nn
-from kemsekov_torch.conv_modules import SCSEModule
+import torch.nn.functional as F
 
 class SeqToVec(nn.Module):
     """
@@ -20,7 +21,7 @@ class SeqToVec(nn.Module):
         final_layer (nn.Module): Convolutional layer to produce the final output vector.
     """
 
-    def __init__(self, in_channels=256, hidden_channels=512, out_channels=512, output_size=(1, 1), block_sizes=[2, 2, 2, 2, 2], dropout_p=0.1):
+    def __init__(self, in_channels=256, hidden_channels=512, out_channels=512, dimensions=1, block_sizes=[2, 2, 2, 2, 2, 2], dropout_p=0.1):
         """
         Initializes the SeqToVec module with the specified parameters.
 
@@ -28,14 +29,13 @@ class SeqToVec(nn.Module):
             in_channels (int): Number of input channels. Default is 256.
             hidden_channels (int): Number of channels in the hidden layers. Default is 512.
             out_channels (int): Number of output channels. Default is 512.
-            output_size (tuple): Desired output size after adaptive pooling. Default is (1, 1).
+            dimensions (int): Input spacial dimensions
             block_sizes (list): List specifying the number of residual blocks in each stage. Default is [2, 2, 2, 2, 2].
             dropout_p (float): Dropout probability. Default is 0.1.
         """
         super().__init__()
 
         # Determine the dimensionality based on the output_size
-        dimensions = len(output_size)
         self.dimensions = dimensions
         self.hidden_channels=hidden_channels
 
@@ -45,26 +45,30 @@ class SeqToVec(nn.Module):
         max_pool = [nn.MaxPool1d, nn.MaxPool2d, nn.MaxPool3d][dimensions - 1]
         conv = [nn.Conv1d, nn.Conv2d, nn.Conv3d][dimensions - 1]
         # bn = nn.SyncBatchNorm
-        bn = [nn.BatchNorm1d,nn.BatchNorm2d,nn.BatchNorm3d][dimensions-1]
+        # bn = [nn.BatchNorm1d,nn.BatchNorm2d,nn.BatchNorm3d][dimensions-1]
         # Positional encoding module
         self.encoder = PositionalEncodingPermute(in_channels)
 
         # Input compression module
         self.compress_input = nn.Sequential(
-            bn(in_channels),
+            nn.BatchNorm2d(in_channels),
             nn.ZeroPad2d((1, 0, 0, 1)),
             nn.Conv2d(in_channels, hidden_channels, kernel_size=2),
-            bn(hidden_channels),
+            nn.ReLU(True),
+            nn.BatchNorm2d(hidden_channels),
         )
 
         # Convolutional layers with residual blocks and squeeze-and-excitation modules
         self.conv_layers = nn.Sequential()
-        for block_size in block_sizes:
+        for i,block_size in enumerate(block_sizes):
+            # to downsample input, add some stride
+            stride = 1+(i%2)
             layer = nn.Sequential(
                 ResidualBlock(
                     hidden_channels,
                     hidden_channels,
                     kernel_size=3,
+                    stride=stride,
                     dimensions=dimensions,
                     conv_impl=[conv] * block_size
                 ),
@@ -83,12 +87,11 @@ class SeqToVec(nn.Module):
             dilations += dilation
 
         self.multiscale_aspp = nn.ModuleList()
-        pyramid_sizes = [1, 2, 4, 8, 16]
-        kernel_sizes = [1, 3, 3, 3, 3]
-        self.min_input_length = max(pyramid_sizes)
-        for s, p in zip(kernel_sizes, pyramid_sizes):
+        kernel_sizes = [3, 3, 3, 3, 3]
+        self.min_input_length = 16
+        for s in kernel_sizes:
             layer = nn.Sequential(
-                max_pool(p, padding=p // 2),
+                max_pool(2, padding=1),
                 ResidualBlock(
                     hidden_channels,
                     hidden_channels,
@@ -99,17 +102,16 @@ class SeqToVec(nn.Module):
                     conv_impl=[conv] * 1
                 ),
                 SCSEModule(hidden_channels, dimensions=dimensions),
-                avg_pool(output_size),
                 dropout(dropout_p)
             )
             self.multiscale_aspp.append(layer)
-
+        self.aspp_pool = avg_pool(1)
         # Final convolutional layer to produce the output vector
         final_conv_kernel = [1] * dimensions
-        final_conv_kernel[-1] = len(pyramid_sizes)
+        final_conv_kernel[-1] = len(kernel_sizes)
 
         final_conv_stride = [1] * dimensions
-        final_conv_stride[-1] = len(pyramid_sizes)
+        final_conv_stride[-1] = len(kernel_sizes)
 
         self.final_layer = conv(hidden_channels, out_channels, kernel_size=final_conv_kernel, stride=final_conv_stride)
 
@@ -125,10 +127,12 @@ class SeqToVec(nn.Module):
         """
         # Apply positional encoding
         positional_emb = self.encoder(x)
-
+        if positional_emb is None:
+            positional_emb=torch.zeros_like(x)
+        
         # Combine input and positional embeddings
         combined = torch.stack([x.flatten(2), positional_emb.flatten(2)], dim=-1)
-
+        
         # Compress the combined input
         compressed_shape = list(x.shape)
         compressed_shape[1] = self.hidden_channels
@@ -140,10 +144,14 @@ class SeqToVec(nn.Module):
         transformed = self.conv_layers(compressed)
 
         # Apply multi-scale ASPP
-        aspp_results = [torch.jit.fork(layer, transformed) for layer in self.multiscale_aspp]
-        result = [torch.jit.wait(res) for res in aspp_results]
-        result = torch.cat(result, dim=-1)
-
+        aspp_results = [transformed]
+        for b in self.multiscale_aspp:
+            v = b(aspp_results[-1])
+            aspp_results.append(v)
+        for v in aspp_results:
+            print(v.shape)
+        aspp_results = [self.aspp_pool(v) for v in aspp_results]
+        aspp_results=torch.concat(aspp_results,dim=-1)
         # Apply final convolutional layer
-        result = self.final_layer(result)
-        return result
+        result = self.final_layer(aspp_results)
+        return result.flatten(1)
