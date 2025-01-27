318f94dcf74520b407e0ea1d879d5d879c06c323
Vlad Kemsekov <pikova98@bk.ru>
2025-01-27 15:40:14 +0500

    Create conv2d_deform.py

---
+++ conv2d_deform.py
@@ -0,0 +1,22 @@
+from torchvision.ops import DeformConv2d
+import torch.nn as nn
+
+# drop-in replacement for ordinary conv
+class Conv2dDeform(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super(DeformableConvNet, self).__init__()
+        offset_out_ch = 2 * kernel_size * kernel_size
+        mask_out_ch = kernel_size * kernel_size
+        self.offset_conv = nn.Conv2d(in_channels, offset_out_ch, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.mask_conv = nn.Conv2d(in_channels, mask_out_ch, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.deform_conv = DeformConv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        if kernel_size//2 == padding and stride==1:
+            self.x_res = nn.Conv2d(in_channels,out_channels,kernel_size=1)
+        else:
+            self.x_res = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+            
+    def forward(self, x):
+        mask = torch.sigmoid(self.mask_conv(x))  # Ensure mask values are between 0 and 1
+        offset = self.offset_conv(x)
+        out = self.deform_conv(x, offset, mask)
+        return self.x_res(x)+out
