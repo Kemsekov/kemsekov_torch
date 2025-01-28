a73bddd2f8d4cfdab44b6d16b608670b62c674a4
Vlad Kemsekov <pikova98@bk.ru>
2025-01-28 08:58:30 +0500

    Update conv2d_deform.py

--- conv2d_deform.py
+++ conv2d_deform.py
@@ -4,12 +4,12 @@ import torch
 
 # drop-in replacement for ordinary conv
 class Conv2dDeform(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1,padding_mode='zeros',dilation=1):
         super(Conv2dDeform, self).__init__()
         offset_out_ch = 2 * kernel_size * kernel_size
         mask_out_ch = kernel_size * kernel_size
-        self.offset_conv = nn.Conv2d(in_channels, offset_out_ch, kernel_size=kernel_size, stride=stride, padding=padding)
-        self.mask_conv = nn.Conv2d(in_channels, mask_out_ch, kernel_size=kernel_size, stride=stride, padding=padding)
+        self.offset_conv = nn.Conv2d(in_channels, offset_out_ch, kernel_size=kernel_size, stride=stride, padding=padding,padding_mode=padding_mode,dilation=dilation)
+        self.mask_conv = nn.Conv2d(in_channels, mask_out_ch, kernel_size=kernel_size, stride=stride, padding=padding,padding_mode=padding_mode,dilation=dilation)
         self.deform_conv = DeformConv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
         if kernel_size//2 == padding and stride==1:
             self.x_res = nn.Conv2d(in_channels,out_channels,kernel_size=1)
