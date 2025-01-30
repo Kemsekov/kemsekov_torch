f52b9207a0e4eb52a0664c88e034eb956c9650c7
Kemsekov <pikova98@bk.ru>
2025-01-30 12:28:23 +0500

    add bsconv for 1d 2d 3d

--- conv_modules.py
+++ conv_modules.py
@@ -124,7 +124,7 @@ class SCSEModule3d(nn.Module):
         # Combine the outputs
         return torch.max(cse_out, sse_out)
 
-class BSConvU(torch.nn.Sequential): 
+class BSConvU(torch.nn.Module): 
     """
     Blueprint Pointwise-Depthwise Convolution Block.
 
@@ -146,10 +146,63 @@ class BSConvU(torch.nn.Sequential):
         dilation (int, optional): Dilation rate for the depthwise convolution. Default is 1.
         bias (bool, optional): Whether to add a learnable bias to the depthwise convolution. Default is True.
         padding_mode (str, optional): Padding mode for the depthwise convolution. Default is 'zeros'.
+        dimensions (int,optional): Convolution dimensions, must be one of [1,2,3]
         with_bn (bool, optional): If True, includes BatchNorm between pointwise and depthwise convolutions. Default is False.
         bn_kwargs (dict, optional): Additional keyword arguments for BatchNorm. Default is None.
     """
 
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, bias=True, padding_mode="zeros",dimensions=2, with_bn=False, bn_kwargs=None): 
+        super().__init__() 
+        assert dimensions in [1,2,3], f"{dimensions} must be in range [1,2,3]"
+        self.conv = [BSConvU1d,BSConvU2d,BSConvU3d][dimensions-1](
+            in_channels=in_channels, 
+            out_channels=out_channels, 
+            kernel_size=kernel_size, 
+            stride=stride, 
+            padding=padding, 
+            dilation=dilation, 
+            bias=bias, 
+            padding_mode=padding_mode,
+            with_bn=with_bn, 
+            bn_kwargs=bn_kwargs
+        )
+    def forward(self,x):
+        return self.conv(x)
+
+class BSConvU1d(torch.nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, bias=True, padding_mode="zeros", with_bn=False, bn_kwargs=None):
+        super().__init__()
+
+        if bn_kwargs is None:
+            bn_kwargs = {}
+
+        self.add_module("pw", torch.nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            dilation=1,
+            groups=1,
+            bias=False,
+        ))
+
+        if with_bn:
+            self.add_module("bn", torch.nn.BatchNorm1d(num_features=out_channels, **bn_kwargs))
+
+        self.add_module("dw", torch.nn.Conv1d(
+            in_channels=out_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=out_channels,
+            bias=bias,
+            padding_mode=padding_mode,
+        ))
+
+class BSConvU2d(torch.nn.Sequential): 
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, bias=True, padding_mode="zeros", with_bn=False, bn_kwargs=None): 
         super().__init__() 
 
@@ -185,3 +238,36 @@ class BSConvU(torch.nn.Sequential):
                 bias=bias, 
                 padding_mode=padding_mode, 
         )) 
+
+class BSConvU3d(torch.nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, bias=True, padding_mode="zeros", with_bn=False, bn_kwargs=None):
+        super().__init__()
+
+        if bn_kwargs is None:
+            bn_kwargs = {}
+
+        self.add_module("pw", torch.nn.Conv3d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            dilation=1,
+            groups=1,
+            bias=False,
+        ))
+
+        if with_bn:
+            self.add_module("bn", torch.nn.BatchNorm3d(num_features=out_channels, **bn_kwargs))
+
+        self.add_module("dw", torch.nn.Conv3d(
+            in_channels=out_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=out_channels,
+            bias=bias,
+            padding_mode=padding_mode,
+        ))
