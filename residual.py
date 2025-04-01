242b53f3c9fbd593d31099a5f5ba375d43456fee
Kemsekov <pikova98@bk.ru>
2025-04-01 16:07:24 +0500

    rework residual block

--- residual.py
+++ residual.py
@@ -51,16 +51,18 @@ class ResidualBlock(torch.nn.Module):
         x_corr_conv_impl = [nn.Conv1d,nn.Conv2d,nn.Conv3d][dimensions-1]
         x_corr_conv_impl_T = [nn.ConvTranspose1d,nn.ConvTranspose2d,nn.ConvTranspose3d][dimensions-1]
             
-        if not isinstance(out_channels,list):
+        if not (isinstance(out_channels,list) or isinstance(out_channels,tuple)):
             out_channels=[out_channels]
-                
+        
         repeats=len(out_channels)
         self.padding_mode=padding_mode
         self._is_transpose_conv = is_transpose
 
         self.normalization = normalization
-        if not isinstance(kernel_size,list):
-            kernel_size=[kernel_size]
+        if not (isinstance(kernel_size,list) or isinstance(kernel_size,tuple)):
+            kernel_size=[kernel_size]*dimensions
+        if not (isinstance(stride,list) or isinstance(stride,tuple)):
+            stride = [stride]*dimensions
 
         self.kernel_size = kernel_size
         
@@ -79,15 +81,6 @@ class ResidualBlock(torch.nn.Module):
         if not isinstance(dilation,list):
             dilation=[dilation]*out_channels[0]
 
-        if len(kernel_size)==1 and len(dilation)!=1:
-            kernel_size=kernel_size*len(dilation)
-        
-        if len(kernel_size)!=1 and len(dilation)==1:
-            dilation=dilation*len(kernel_size)
-
-
-        # assert len(dilation) == out_channels[0], f"Number of dilations must match the number of output channels at first dim. {len(dilation)} != {out_channels[0]}"
-
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.stride = stride
@@ -97,40 +90,29 @@ class ResidualBlock(torch.nn.Module):
         
         # collapse same-shaped conv blocks to reduce computation resources
         out_channels_ = [1]
-        kernel_sizes_ = [kernel_size[0]]
         dilations_ = [dilation[0]]
         
-        for c_size,c_dilation in list(zip(kernel_size,dilation))[1:]:
-            if c_size==kernel_sizes_[-1] and c_dilation==dilations_[-1]:
+        for c_dilation in (dilation)[1:]:
+            if c_dilation==dilations_[-1]:
                 out_channels_[-1]+=1
             else:
                 out_channels_.append(1)
-                kernel_sizes_.append(c_size)
                 dilations_.append(c_dilation)
         # collapse same-shaped conv blocks to reduce computation resources
-        # for non-first layers
-        out_channels_without_dilation = [1]
-        kernel_sizes_without_dilation = [kernel_size[0]]
-            
-        for c_size in list(kernel_size)[1:]:
-            if c_size==kernel_sizes_without_dilation[-1]:
-                out_channels_without_dilation[-1]+=1
-            else:
-                out_channels_without_dilation.append(1)
-                kernel_sizes_without_dilation.append(c_size)
-        assert len(kernel_size)==len(dilation), f"len(kernel_size) must equal len(dilation), {len(kernel_size)} != {len(dilation)}"
         
         self.convs = []
         self.norms = []
         self.input_resize=nn.ModuleList([torch.nn.Identity()]*repeats)
         
-        assert stride%2==0 or stride==1, f"stride must be even or 1. given stride={stride}"
-        strides = [1]*repeats
+        assert all([s%2==0 or s==1 for s in stride]), f"all stride must be even or 1. given stride={stride}"
         
-        stride_ind=0
-        while math.prod(strides)<stride:
-            strides[stride_ind%repeats]*=2
-            stride_ind+=1
+        strides = torch.tensor([[1]*dimensions]*repeats)
+        
+        for d in range(dimensions):
+            stride_ind=0
+            while math.prod([v[d] for v in strides])<stride[d]:
+                strides[stride_ind%repeats][d]*=2
+                stride_ind+=1
         
         for v in range(repeats):
             # on first repeat block make sure to cast input tensor to output shape
@@ -139,52 +121,57 @@ class ResidualBlock(torch.nn.Module):
             stride_ = strides[v]
 
             # do not change
-            dil = dilations_ if v==0 else [1]*len(out_channels_without_dilation)
+            dil = dilations_ if v==0 else [1]*len(dilations_)
             
             # change to match out_channels[v]
-            outc = out_channels_ if v==0 else out_channels_without_dilation
+            outc = out_channels_
             outc = [int(c/sum(outc)*out_channels[v]) for c in outc]
             
             # channels that is remained to be added
             remaining_channels = out_channels[v]-sum(outc)
             
             # do not change
-            ksizes = kernel_sizes_ if v==0 else kernel_sizes_without_dilation
-            outc[torch.argmin(torch.tensor(ksizes))]+=remaining_channels
+            outc[torch.argmin(torch.tensor(dilations_))]+=remaining_channels
             
             # Store the conv layers for each output channel with different dilations.
             convs_ = []
             for i in range(len(outc)):
-                ks = ksizes[i]
+                ks = torch.tensor(kernel_size)
                 # actual conv kernel size with dilation
 
-                if ks%2==0:
-                    if v==0:
-                        assert stride_!=1,f"Impossible to use kernel_size={ks} with stride=1 to get same-shaped tensor"
-                    elif stride_==1:
-                        ks-=1
+                for d in range(dimensions):
+                    if ks[d]%2==0:
+                        if v==0:
+                            assert stride_[d]!=1,f"Impossible to use kernel_size={ks} with stride=1 to get same-shaped tensor"
+                        elif stride_[d]==1:
+                            ks[d]-=1
                         
                 ks_with_dilation = ks + (ks - 1) * (dil[i] - 1)
-                
-                if ks_with_dilation%2==0:
-                    compensation = -1
-                else:
-                    compensation=0
+                compensation = torch.zeros_like(ks_with_dilation)
+                for d in range(dimensions):
+                    if ks_with_dilation[d]%2==0:
+                        compensation[d] = -1
+                    else:
+                        compensation[d] = 0
                 
                 conv_kwargs = dict(
                     in_channels=in_ch,
                     out_channels=outc[i],
-                    kernel_size=ks,
-                    padding = ks_with_dilation // 2 + compensation,
+                    kernel_size=ks.tolist(),
+                    padding = (ks_with_dilation // 2 + compensation).tolist(),
                     dilation=dil[i],
-                    stride=stride_,
+                    stride=stride_.tolist(),
                     padding_mode=padding_mode
                 )
 
                 conv__ = x_corr_conv_impl
                 # for downsampling use convolutions to extract features
-                if stride_!=1 and self._is_transpose_conv:
-                    conv_kwargs['output_padding']=stride_ - 1 + compensation
+                if self._is_transpose_conv:
+                    conv_kwargs['output_padding'] = torch.zeros_like(stride_)
+                    for d in range(dimensions):
+                        if stride_[d]!=1:
+                            conv_kwargs['output_padding'][d]=stride_[d] - 1 + compensation[d]
+                    conv_kwargs['output_padding'] = conv_kwargs['output_padding'].tolist()
                     conv__ = x_corr_conv_impl_T
                 convs_.append(conv__(**conv_kwargs))
                 
@@ -218,32 +205,37 @@ class ResidualBlock(torch.nn.Module):
     def _conv_x_linear(self, in_channels, out_channels, stride, norm_impl, x_corr_conv_impl,x_corr_conv_impl_T):
         # compute x_size correction convolution arguments so we could do residual addition when we have changed
         # number of channels or some stride
-        correct_x_ksize = 1
-        if stride!=1:
-            if stride%2==0:
-                correct_x_ksize=stride+1
-            else:
-                correct_x_ksize=stride*2-1
-                
-            if self._is_transpose_conv:
-                correct_x_ksize=stride*2
-            
-        correct_x_padding= correct_x_ksize // 2
-        if correct_x_ksize%2==0:
-            compensation = -1
-        else:
-            compensation = 0
-            
-        correct_x_dilation = 1
+        # correct_x_ksize = [1]*self.dimensions
+        correct_x_ksize = self.kernel_size
+        # for d in range(self.dimensions):
+        #     if stride[d]!=1:
+        #         if stride[d]%2==0:
+        #             correct_x_ksize[d]=stride[d]+1
+        #         else:
+        #             correct_x_ksize[d]=stride[d]*2-1
+                    
+        #         if self._is_transpose_conv:
+        #             correct_x_ksize[d]=stride[d]*2
         
+        correct_x_ksize = torch.tensor(correct_x_ksize)
+        correct_x_padding= correct_x_ksize // 2
+        compensation = torch.zeros_like(correct_x_ksize)
+        for d in range(self.dimensions):
+            if correct_x_ksize[d]%2==0:
+                compensation[d] = -1
+            else:
+                compensation[d] = 0
+                
+        correct_x_dilation = 1
+        stride = torch.tensor(stride)
         # make cheap downscale
         x_corr_kwargs=dict(
             in_channels=in_channels,
             out_channels=out_channels,
-            kernel_size = correct_x_ksize,
+            kernel_size = correct_x_ksize.tolist(),
             dilation=correct_x_dilation,
-            stride = stride,
-            padding = correct_x_padding+compensation,
+            stride = stride.tolist(),
+            padding = (correct_x_padding+compensation).tolist(),
             padding_mode=self.padding_mode
             # groups=gcd(in_channels,out_channels)
         )
@@ -251,12 +243,12 @@ class ResidualBlock(torch.nn.Module):
         x_conv_impl = x_corr_conv_impl
         if self._is_transpose_conv:
             x_conv_impl = x_corr_conv_impl_T
-            x_corr_kwargs['output_padding'] = stride - 1 + compensation
+            x_corr_kwargs['output_padding'] = (stride - 1 + compensation).tolist()
             x_corr_kwargs['groups'] = 1
         
         # if we have different output tensor size, apply linear x_linearion
         # to make sure we can add it with output
-        if stride>1 or in_channels!=out_channels:
+        if any([s>1 for s in stride]) or in_channels!=out_channels:
             # there is many ways to linearly downsample x, but max pool with conv2d works best of all
             self.x_linear = \
                 torch.nn.Sequential(
