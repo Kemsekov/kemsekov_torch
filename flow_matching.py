220b2097a38b821d3a4da12b59827d12e6a4d608
Kemsekov <pikova98@bk.ru>
2026-05-15 17:59:25 +0500

    Keep tanh

--- flow_matching.py
+++ flow_matching.py
@@ -490,6 +490,7 @@ class FusedFlowResidual(nn.Module):
         self.prod = nn.Sequential(
             nn.SiLU(),
             nn.Linear(hidden_dim,hidden_dim,bias=False),
+            nn.Tanh()
         )
         self.out = nn.Sequential(
             nn.SiLU(),
