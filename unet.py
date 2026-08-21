05a7f4cf8ce5d996ea40bd55e90d120153e8d5da
Kemsekov <pikova98@gmail.com>
2026-08-21 10:59:03 +0500

    update unet scheme

--- unet.py
+++ unet.py
@@ -8,13 +8,10 @@ def sum_tensors(a,b):
     return a+b
 
 class Unet(nn.Module):
-    def __init__(self, in_channels,out_channels,compression = 4,layers_scaler=1,dropout=0):
+    def __init__(self, in_channels,out_channels,compression = 4,layer_dims = [32,64,128,256],dropout=0):
         super().__init__()
         self.in_channels=in_channels
         self.out_channels=out_channels
-        
-        layer_dims = [32,64,128,256]
-        for i in range(4): layer_dims[i]=int(layer_dims[i]*layers_scaler)
         
         self.compress = nn.Sequential(
             nn.PixelUnshuffle(compression),
@@ -44,12 +41,13 @@ class Unet(nn.Module):
                 ]),
                 nn.SiLU()
             )
-        self.down1 = down_block(layer_dims[0],layer_dims[1])
-        self.down2 = down_block(layer_dims[1],layer_dims[2])
-        self.down3 = down_block(layer_dims[2],layer_dims[3])
+        downs = []
+        for prev_l,next_l in zip(layer_dims,layer_dims[1:]):
+            downs.append(down_block(prev_l,next_l))
+        self.downs = nn.ModuleList(downs)
         
         self.attn = nn.Sequential(
-            SelfAttention(layer_dims[-1],add_rotary_embedding=True,add_absolute_pos=True,dimensions=2,xsa=True),
+            SelfAttention(layer_dims[-1],add_rotary_embedding=True,add_absolute_pos=False,dimensions=2,xsa=True),
             nn.GroupNorm(layer_dims[-1]//16,layer_dims[-1]),
             Prod([
                 nn.Conv2d(layer_dims[-1],layer_dims[-1],1),
@@ -58,10 +56,10 @@ class Unet(nn.Module):
             nn.Dropout(dropout),
             nn.SiLU(),
         )
-        
-        self.up1 = up_block(layer_dims[3],layer_dims[2])
-        self.up2 = up_block(layer_dims[2],layer_dims[1])
-        self.up3 = up_block(layer_dims[1],layer_dims[0])
+        ups = []
+        for prev_l,next_l in reversed(list(zip(layer_dims,layer_dims[1:]))):
+            ups.append(up_block(next_l,prev_l))
+        self.ups = nn.ModuleList(ups)
         
         self.final = nn.Sequential(
             nn.Conv2d(layer_dims[0],compression**2*out_channels,kernel_size=3,padding=1),
@@ -70,15 +68,39 @@ class Unet(nn.Module):
         
     def forward(self,x):
         x = self.compress(x)
-        d1 = self.down1(x)
-        d2 = self.down2(d1)
-        d3 = self.down3(d2)
+        
+        down_act = []
+        d = x
+        for down in self.downs:
+            down_act.append(d)
+            d = down(d)
+        
+        u = self.attn(d)
+        # ups = [u]
+        for up,d in zip(self.ups,reversed(down_act)):
+            u = sum_tensors(d,up(u))
+            # ups.append(u)
+        
+        # print("DOWNS")
+        # for d in down_act:
+        #     print(d.shape)
+        # print("UPS")
+        # for d in ups:
+        #     print(d.shape)
+        
+        return self.final(u)
+        
+        x = self.compress(x)
+        
+        d1 = self.downs[0](x)
+        d2 = self.downs[1](d1)
+        d3 = self.downs[2](d2)
         
         bottom = self.attn(d3)
         
-        u1 = self.up1(bottom)
-        u2 = sum_tensors(d1,self.up2(u1))
-        u3 = sum_tensors(x,self.up3(u2))
+        u1 = sum_tensors(d2,self.ups[0](bottom))
+        u2 = sum_tensors(d1,self.ups[1](u1))
+        u3 = sum_tensors(x,self.ups[2](u2))
         
         return self.final(u3)
   
